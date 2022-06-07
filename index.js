d68619d678019d66061c60ee0ce4b28059fc7696
Daniel Stephen <73526667+DanStep65@users.noreply.github.com>
2022-06-07 05:52:34 +0000

    Installed multer pakcage, used multer package to return the requested data in json

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var fs = require('fs');
+//just use multer package to handle file uploading
+var multer = require('multer');
+
+const upload = multer({dest: 'uploads/'});
+
+require('dotenv').config();
 
 var app = express();
 
@@ -10,6 +16,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  const file = req.file;
+  res.json(
+    {
+      name: file['originalname'], 
+      type: file['mimetype'], 
+      size: file['size']});
+});
+
 
 
 
